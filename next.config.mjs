2fdf45cb19742a0992fdd23cf3029200cd16d640
tobiadewiz-6124 <tobiadewiz@gmail.com>
2025-10-04 02:47:43 +0100

    Simplify config to debug

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Remove 'standalone' for Vercel deployments
-  
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-      allowedOrigins: ['*'], // Merged both experimental configs
-    },
-  },
-  
   eslint: { 
     ignoreDuringBuilds: true 
   },
